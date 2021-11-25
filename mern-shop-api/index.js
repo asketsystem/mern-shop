7978388186f3acf03548cdc9e386a2fc1faeb1e6
asketsystem <asketsystem1@gmail.com>
2021-11-25 08:50:26 +0000

    MongoDB connection successful

--- mern-shop-api/index.js
+++ mern-shop-api/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+mongoose
+    .connect("mongodb+srv://mernapi:mernapi@cluster0.vg0x5.mongodb.net/shop?retryWrites=true&w=majority"
+    )
+    .then(() => console.log("DBConnection Successfull!"))
+    .catch((err) => {
+        console.log(err);
+    });
+
 app.listen(5000, () => {
     console.log("Backend server is running!");
 });
