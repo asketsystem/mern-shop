0ad5c20eff72c9b5b08a8d6a339d9fd5b562a43a
asketsystem <asketsystem1@gmail.com>
2021-11-25 09:06:30 +0000

    Complete MongoDB connection

--- mern-shop-api/index.js
+++ mern-shop-api/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 mongoose
-    .connect("mongodb+srv://mernapi:mernapi@cluster0.vg0x5.mongodb.net/shop?retryWrites=true&w=majority"
-    )
+    .connect(process.env.MONGO_URL)
     .then(() => console.log("DBConnection Successfull!"))
     .catch((err) => {
         console.log(err);
     });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!");
 });
