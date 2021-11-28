faf31dcb9af24a569abd3d540743169d0afb5f6f
asketsystem <asketsystem1@gmail.com>
2021-11-28 15:30:45 +0000

    JWT Implementation

--- mern-shop-api/routes/auth.js
+++ mern-shop-api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -24,6 +25,12 @@ router.post("/login", async (req, res) => {
         const user = await User.findOne({ username: req.body.username });
         !user && res.status(401).json("Wrong credentials!")
 
+        const accessToken = jwt.sign({
+            id:user._id, isAdmin: user.isAdmin,
+        }, process.env.JWT_SEC,
+        {expiresIn:"3d"}
+        );
+
         const hashedPassword = CryptoJS.AES.decrypt(
             user.password,
             process.env.PASS_SEC
@@ -35,7 +42,7 @@ router.post("/login", async (req, res) => {
         
         const { password, ...others } = user._doc;
 
-        res.status(200).json(others);
+        res.status(200).json({...others, accessToken});
     } catch (err) {
       res.status(500).json(err);
     }
